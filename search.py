f483374eb8a5910acd291aa02c546667ed58f9a6
nitish770 <nitishkmahto257@gmail.com>
2021-03-14 10:06:08 +0530

    colorful output added

--- search.py
+++ search.py
@@ -1,6 +1,22 @@
 import re
 import requests
 from helpers.logger import *
+import random
+
+def randomColor():
+	colors = {
+		'BLUE': '\x1b[34m', 'CYAN': '\x1b[36m',
+		'GREEN': '\x1b[32m', 'LIGHTBLACK_EX': '\x1b[90m', 'LIGHTBLUE_EX': '\x1b[94m',
+		'LIGHTCYAN_EX': '\x1b[96m', 'LIGHTGREEN_EX': '\x1b[92m', 'LIGHTMAGENTA_EX': '\x1b[95m',
+		'LIGHTWHITE_EX': '\x1b[97m', 'LIGHTYELLOW_EX': '\x1b[93m',
+		'MAGENTA': '\x1b[35m', 'RED': '\x1b[31m', 'RESET': '\x1b[39m', 'WHITE': '\x1b[37m',
+		'YELLOW': '\x1b[33m'
+			}
+
+	return colors[random.choices([i for i in colors.keys()])[0]]
+
+def resetColor():
+	print('\x1b[97m')
 
 
 def changeQuote(words:list):
@@ -33,20 +49,24 @@ def getMeaning(word):
 		pronounce  = ptrn_pronounce.findall(r.text)
 		pronounce_ = list(set([i[2:-1] for i in pronounce]))
 
-		print("Pronunciation : ")
+		print("\x1b[91mPronunciation : \x1b[97m")
+		resetColor()
+
 		if pronounce:
 			for i in pronounce_:
-				print('\t', count, i)
+				print('\t', count, randomColor() + i)
+				resetColor()
 				count += 1
 
 		mean = ptrn_mean.findall(r.text)
 		mean_ = [i[1:-1] for i in mean]
 
 		count = 1
-		print("\nMean : ")
+		print("\n\x1b[91mMean : \x1b[97m")
 		if mean:
 			for i in mean_:
-				print('\t', count, i)
+				print('\t', count, randomColor() + i)
+				resetColor()
 				count+=1
 
 		successful(word, pronounce_, mean_)
